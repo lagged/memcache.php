936c30501c7ca0cf3fee663a667dc467e164ae4e
till <till@php.net>
2012-04-13 18:24:14 +0200

    MEMCACHE_SCRIPT, defaults to PHP_SELF

--- etc/config.php
+++ etc/config.php
@@ -9,6 +9,7 @@ define('ADMIN_PASSWORD','pass');    // Admin Password
 define('DATE_FORMAT','Y/m/d H:i:s');
 define('GRAPH_SIZE',200);
 define('MAX_ITEM_DUMP',50);
+define('MEMCACHE_SCRIPT', $_SERVER['PHP_SELF']);
 
 $MEMCACHE_SERVERS[] = 'localhost:11211'; // add more as an array
 //$MEMCACHE_SERVERS[] = 'mymemcache-server2:11211'; // add more as an array
