2f320d0db80c68ff27f3b2af2c73543a57f2cc20
till <till@php.net>
2012-04-13 16:26:32 +0200

    only prompt for username/password when we actually require one

--- memcache.php
+++ memcache.php
@@ -25,18 +25,23 @@ require_once __DIR__ . '/etc/config.php';
 ////////// END OF DEFAULT CONFIG AREA /////////////////////////////////////////////////////////////
 
 ///////////////// Password protect ////////////////////////////////////////////////////////////////
-if (!isset($_SERVER['PHP_AUTH_USER']) || !isset($_SERVER['PHP_AUTH_PW']) ||
-           $_SERVER['PHP_AUTH_USER'] != ADMIN_USERNAME ||$_SERVER['PHP_AUTH_PW'] != ADMIN_PASSWORD) {
-			Header("WWW-Authenticate: Basic realm=\"Memcache Login\"");
-			Header("HTTP/1.0 401 Unauthorized");
+if (defined('ADMIN_USERNAME') && ADMIN_USERNAME != '') {
+    if (!isset($_SERVER['PHP_AUTH_USER'])
+        || !isset($_SERVER['PHP_AUTH_PW'])
+        || $_SERVER['PHP_AUTH_USER'] != ADMIN_USERNAME
+        || $_SERVER['PHP_AUTH_PW'] != ADMIN_PASSWORD
+    ) {
+        Header("WWW-Authenticate: Basic realm=\"Memcache Login\"");
+        Header("HTTP/1.0 401 Unauthorized");
 
-			echo <<<EOB
-				<html><body>
-				<h1>Rejected!</h1>
-				<big>Wrong Username or Password!</big>
-				</body></html>
+        echo <<<EOB
+<html><body>
+    <h1>Rejected!</h1>
+	<big>Wrong Username or Password!</big>
+</body></html>
 EOB;
-			exit;
+        exit(1);
+    }
 }
 
 ///////////MEMCACHE FUNCTIONS /////////////////////////////////////////////////////////////////////
